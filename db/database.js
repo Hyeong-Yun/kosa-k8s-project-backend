a3700c1b4956deced2fb295a1852d27eec5fa042
hyeongyun <guddbs5545@naver.com>
2023-07-12 23:16:57 +0900

    DB 다시 추가

--- db/database.js
+++ db/database.js
@@ -1,21 +1,11 @@
-import mysql from 'mysql';
+import mysql from 'mysql2';
+import { config } from '../config.js';
 
-export const config = {
-    db: {
-      host: "database-1.cghdvtao1swe.ap-northeast-2.rds.amazonaws.com",
-      user: "admin",
-      databse: "toys",
-      password: "nwmc1023!!",
-    },
-  };
-
-const pool = mysql.createConnection({
+const pool = mysql.createPool({
   host: config.db.host,
   user: config.db.user,
   database: config.db.databse,
   password: config.db.password,
 });
-
-pool.connect(); 
 
 export const db = pool.promise();
