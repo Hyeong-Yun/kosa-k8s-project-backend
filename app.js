5d7844378f2fce729d1bf4cc75a9ecf47ba9521d
hyeongyun <guddbs5545@naver.com>
2023-07-13 18:43:05 +0900

    라우터 product로 변경

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(helmet());
 app.use(cors());
 app.use(morgan('tiny'));
 
-app.use('/items', tweetRouter);
+app.use('/products', tweetRouter);
 // app.use('/auth', authRouter);
 
 app.use((req, res, next) => {
